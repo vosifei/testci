53a639e0404c3d1da1476939dc4c05f981939101
vosifei <vosifei@protonmail>
2017-11-07 00:03:28 +0900

    Revert "chg sed"
    This reverts commit b777c590007538c6c932f581327059ad7adc19d9.

--- .travis-init.sh
+++ .travis-init.sh
@@ -87,8 +87,8 @@ run_install() {
   cd $PATH_TO_REDMINE
 
   #--- added for ui test ---
-  sed '/capybara/d' ./Gemfile
-  sed '/selenium-webdriver/d' ./Gemfile
+  sed '/capybara/d' Gemfile
+  sed '/selenium-webdriver/d' Gemfile
 
   # create a link to the plugin, but avoid recursive link.
   if [ -L "$PATH_TO_PLUGINS/$PLUGIN" ]; then rm "$PATH_TO_PLUGINS/$PLUGIN"; fi
