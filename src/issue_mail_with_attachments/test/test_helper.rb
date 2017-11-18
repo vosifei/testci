18317a8eead139f2a112fd77c9de9ed521a64a10
vosifei <vosifei@protonmail>
2017-11-18 11:16:28 +0900

    no ui test

--- src/issue_mail_with_attachments/test/test_helper.rb
+++ src/issue_mail_with_attachments/test/test_helper.rb
@@ -3,7 +3,7 @@ if ENV['COVERALL4MYPLUGIN'] == 'true'
   require 'simplecov'
   require 'coveralls'
   SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-  SimpleCov.merge_timeout 3600
+  #SimpleCov.merge_timeout 3600
   SimpleCov.start do
      add_filter do |source_file|
        !source_file.filename.include? "/plugins/"
