06fb8101059c14ce663c462157546568df2c9fee
vosifei <vosifei@protonmail>
2019-08-18 00:38:45 +0900

    divide test

--- .travis-init.sh
+++ .travis-init.sh
@@ -64,8 +64,14 @@ run_tests() {
     TRACE=--trace
   fi
 
-  echo "--- test start ------------------------"
-  script -e -c "RUBYOPT=-W0 COVERALL4MYPLUGIN=true bundle exec rake redmine:plugins:test NAME="$PLUGIN $VERBOSE
+  echo "--- unit test start ------------------------"
+  script -e -c "RUBYOPT=-W0 COVERALL4MYPLUGIN=true bundle exec rake redmine:plugins:test:units NAME="$PLUGIN $VERBOSE
+
+  echo "--- function test start ------------------------"
+  script -e -c "RUBYOPT=-W0 COVERALL4MYPLUGIN=true bundle exec rake redmine:plugins:test:functionals NAME="$PLUGIN $VERBOSE
+
+  echo "--- integration test start ------------------------"
+  script -e -c "RUBYOPT=-W0 COVERALL4MYPLUGIN=true bundle exec rake redmine:plugins:test:integration NAME="$PLUGIN $VERBOSE
 
   echo "--- UI test start ------------------------"
   script -e -c "RUBYOPT=-W0 bundle exec rake test TEST=plugins/$PLUGIN/test/ui/**/*_test.rb" $VERBOSE
